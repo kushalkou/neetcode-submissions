3f52371bea765dd1fd3c8e3ac645d73280d8a9bc
Kushal <kushal2005k@gmail.com>
2026-09-01 19:12:15 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        unordered_map<char, int> countS;
+        unordered_map<char, int> countT;
+
+        for(int i=0; i<s.length(); i++){
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+        
+        if(countS == countT){
+            return 1;
+        }
+        else
+            return 0;
+    }
+};
